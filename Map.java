992512da772f3694c2d5ac3de499020418b7fbb6
ViNguyen31 <80879169+ViNguyen3@users.noreply.github.com>
2021-10-05 22:13:45 +0000

    Updating storing char into array

--- Map.java
+++ Map.java
@@ -9,16 +9,41 @@ class Map
   // read from file - Done 
   public static void main(String[] args) throws FileNotFoundException
   {
-    String[][] map = new String[5][];
+    char[][] map = new char[5][];
     Scanner in = new Scanner(new File("Area3.txt"));
     int counter = 0;
     int i = 0,j = 0;
     while (in.hasNextLine())
     {
-      String currentLine = in.next();
-      System.out.print(currentLine);
+      String currentLine = in.nextLine();
+      //loop go through current line 
+      for(int z = 0; z < currentLine.length()-1; z++)
+      { 
+        i = 0;
+        if(Character.isLetter(currentLine.charAt(z)))
+        {
+          System.out.println(map[i][j]);
+          map[j][i] = currentLine.charAt(z);
+          i++;
+        }
+        j++;
+      }
     }
     in.close();
 
+    // for (int a = 0; i < map.length-1; i++)
+    // {
+    //   for(int b = 0; b < map[a].length; b++)
+    //   {
+    //     System.out.print(map[a][b]);
+    //   }
+    // }
+
+    // String test = "He Llo"; 
+    // System.out.println(test.length()); 
+    // for(int i = 0; i < test.length()-1;i++)
+    // {
+    //   System.out.println(test.charAt(i));
+    // }
   }
 }
