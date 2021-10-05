14463e44592caa7716c6fd37d3fd8e021a02daa0
ViNguyen31 <80879169+ViNguyen3@users.noreply.github.com>
2021-10-05 23:36:52 +0000

    updated Map, done with inserting array

--- Map.java
+++ Map.java
@@ -9,7 +9,7 @@ class Map
   // read from file - Done 
   public static void main(String[] args) throws FileNotFoundException
   {
-    char[][] map = new char[5][];
+    char[][] map = new char[5][5];
     Scanner in = new Scanner(new File("Area3.txt"));
     int counter = 0;
     int i = 0,j = 0;
@@ -19,25 +19,30 @@ class Map
       //loop go through current line 
       for(int z = 0; z < currentLine.length()-1; z++)
       { 
-        i = 0;
         if(Character.isLetter(currentLine.charAt(z)))
         {
-          System.out.println(map[i][j]);
           map[j][i] = currentLine.charAt(z);
           i++;
         }
-        j++;
+        //   System.out.println(map[i][j]);
+        //   map[j][i] = currentLine.charAt(z);
+        //   i++;
+        if(i == 4)
+        {
+          j++; 
+          i = 0;
+        }
       }
     }
     in.close();
 
-    // for (int a = 0; i < map.length-1; i++)
-    // {
-    //   for(int b = 0; b < map[a].length; b++)
-    //   {
-    //     System.out.print(map[a][b]);
-    //   }
-    // }
+    for (int a = 0; i < map.length-1; i++)
+    {
+      for(int b = 0; b < map[a].length; b++)
+      {
+        System.out.println(map[a][b]);
+      }
+    }
 
     // String test = "He Llo"; 
     // System.out.println(test.length()); 
--- Water.java
+++ Water.java
@@ -1,4 +1,10 @@
-public interface Water 
-{
+public interface Water {
+  public String specialMenu = "1. ";
   
+  //yea its just that im in lab rn and she going over this part, so im just kinda doing notes and stuff. yup np
+  public String waterGun(Pokemon p);
+
+  public String bubbleBeam(Pokemon p);
+
+  public String waterfall(Pokemon p);
 }
